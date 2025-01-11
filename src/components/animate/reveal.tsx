bcaf695aadd06aa5e8bd1f809d125b416fd09f74
ahbren <125674024+BrendanNKD@users.noreply.github.com>
2025-01-11 17:32:56 +0800

    Fix ESLint warning

--- src/components/animate/reveal.tsx
+++ src/components/animate/reveal.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useRef } from "react";
 
 import { motion, useAnimation, useInView } from "framer-motion";
@@ -6,35 +6,38 @@ import { motion, useAnimation, useInView } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 interface Props {
-	children: JSX.Element;
-  	className?: string;
-	width?: "fit-content" | "100%" | "50%";
+  children: JSX.Element;
+  className?: string;
+  width?: "fit-content" | "100%" | "50%";
 }
 
 const Reveal = ({ children, className, width = "fit-content" }: Props) => {
-	const ref = useRef(null);
-	const isInView = useInView(ref, { once: true });
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-	const animate = useAnimation();
+  const animate = useAnimation();
 
-	useEffect(() => {
-		if (isInView) {
-			animate.start("visible");
-		}
-	}, [isInView]);
+  useEffect(() => {
+    if (isInView) {
+      animate.start("visible");
+    }
+  }, [isInView, animate]); // Include `animate` in the dependency array
 
-	return (
-		<motion.div
-			className={cn(className, "relative overflow-hidden")}
-			style={{ width }}
-			variants={{
-				hidden: { opacity: 0, y: 25 },
-				visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-			}}
-		>
-			{children}
-		</motion.div>
-	);
+  return (
+    <motion.div
+      ref={ref} // Attach the ref to the motion.div
+      className={cn(className, "relative overflow-hidden")}
+      style={{ width }}
+      initial="hidden"
+      animate={animate} // Use the `animate` object here
+      variants={{
+        hidden: { opacity: 0, y: 25 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default Reveal;
